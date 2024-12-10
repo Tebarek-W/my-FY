e010f95c0bba78d05b977369b5327923cde9d517
Tebarek Wachamo <tebarekwache@gmail.com>
2024-12-10 15:57:05 +0300

    Your commit message

---
+++ src/common/index.js
@@ -0,0 +1,21 @@
+
+
+const backendDomain = "http://localhost:8080"
+
+
+const SummaryApi = {
+  signUp : {
+    url: `${backendDomain}/api/signup`,
+    method : "post"
+  },
+  signIn : {
+    url: `${backendDomain}/api/signin`,
+    method : "post"
+  },
+  current_user : {
+    url : `${backendDomain}/api/user-details`,
+    method : "get"
+  }
+}
+
+export default SummaryApi
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react'
 import loginIcons from '../assest/signin.gif'
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import imageTobase64 from '../helpers/imageTobase64';
+import SummaryApi from '../common/index';
+import { toast } from 'react-toastify';
 
 const SignUp = () => {
 
@@ -16,6 +18,8 @@ const SignUp = () => {
     confirmPassword : "",
     profilePic : ""
    })
+
+   const navigate = useNavigate()
 
    const handleOnChange = (e) =>{
     const { name , value } = e.target
@@ -43,12 +47,34 @@ const SignUp = () => {
    }
 
 
-   const handleSubmit = (e) => {
+   const handleSubmit = async(e) => {
     e.preventDefault()
+
+    if(data.password === data.confirmPassword){
+      console.log("SummaryApi.signUp.url",SummaryApi.signUp.url)
+
+      const dataResponse = await fetch(SummaryApi.signUp.url,{
+        method : SummaryApi.signUp.method,
+        headers : {
+          "content-type" : "application/json"
+        },
+        body : JSON.stringify(data)
+      })
+      const dataApi = await dataResponse.json()
+
+      if(dataApi.success){
+        toast.success(dataApi.message)
+        navigate("/login")
+      }
+
+      if(dataApi.error){
+        toast.error(dataApi.message)
+      }
+
+    }else{
+      console.log("Password doesn't match!")
+    }
    }
-
-   console.log("data login", data)
-
 
   return (
     <section id='login'>
